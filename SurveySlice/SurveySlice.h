6269ce4e6eb6f07183c22af982450a31403bd670
favasian06 <christopher@boomerang.io>
2018-02-28 09:16:41 -0800

    Initial Commit

---
+++ SurveySlice/SurveySlice.h
@@ -0,0 +1,19 @@
+//
+//  SurveySlice.h
+//  SurveySlice
+//
+//  Created by Christopher Li on 2/28/18.
+//  Copyright © 2018 Apps thay Pay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SurveySlice.
+FOUNDATION_EXPORT double SurveySliceVersionNumber;
+
+//! Project version string for SurveySlice.
+FOUNDATION_EXPORT const unsigned char SurveySliceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SurveySlice/PublicHeader.h>
+
+
