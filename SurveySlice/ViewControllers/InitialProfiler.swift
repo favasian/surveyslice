894862f8d8bfcc1d027fc93dd602b696e675060a
favasian06 <christopher@boomerang.io>
2018-02-28 13:19:43 -0800

    try this

--- SurveySlice/ViewControllers/InitialProfiler.swift
+++ SurveySlice/ViewControllers/InitialProfiler.swift
@@ -8,30 +8,16 @@
 
 import UIKit
 
-public class InitialProfiler: UIViewController {
+class InitialProfiler: UIViewController {
 
-    public override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-    }
-
-    public override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
     
     public class func hello() {
         print("hiiiiiii!!!!")
     }
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
